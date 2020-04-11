37717648c3a3ecce3ee9a4f23645c72c15b89482
7slcode7 <slahiruc007@gmail.com>
2020-04-11 18:15:25 +0530

    default and static

--- Chapter1/src/Chap_2/Car.java
+++ Chapter1/src/Chap_2/Car.java
@@ -1,6 +1,17 @@
 package Chap_2;
 
-public class Car extends Vehicle implements Changeable{
+interface Changeable2 {
+    public default void nextDefaultMethod(){
+        System.out.println("Changeable 2 default method.");
+    }
+}
+public class Car extends Vehicle implements Changeable , Changeable2{
+
+
+    public void nextDefaultMethod(){
+        System.out.println("Wow");
+    }
+
     public static void main(String[] args) {
         Changeable ch = new Car();
         ch.defaultMethod();
