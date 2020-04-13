3f73ac9a772cccd0597bbad508c6fa8be30459a0
7slcode7 <slahiruc007@gmail.com>
2020-04-13 23:31:53 +0530

    sample

---
+++ Chapter1/src/Chap_1/NewYear.java
@@ -0,0 +1,8 @@
+package Chap_1;
+
+public class NewYear {
+    public static void main(String[] args) {
+        System.out.println("Wish you a Happy New Year !!!");
+        System.out.println("I am going to do my best in this year !!!");
+    }
+}
